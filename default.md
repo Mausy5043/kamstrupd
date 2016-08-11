6e00ebef838459196647f218495546cd0d0979b1
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-08-11 15:17:10 +0200

    additional daemons

---
+++ default.md
@@ -0,0 +1,27 @@
+---
+title: Stream
+---
+# The Stream
+
+This page is all about the stream. The stream is measured by a KAMSTRUP 162JxC smart electricity meter.
+A Raspberry Pi 1B reads the measurement data via the serialport of the meter.
+
+<style type="text/css">
+
+.zoomer {
+  border: none;
+  margin: 0 auto;
+  transition: all 0.5s ease;
+  -webkit-transition: all 0.5s ease;
+  -moz-transition: all 0.5s ease;
+  -ms-transition: all 0.5s ease;
+}
+
+.zoomer:hover {
+  -webkit-transform: scale(1.5);
+  -moz-transform: scale(1.5);
+  -o-transform: scale(1.5);
+  transform: scale(1.5);
+}
+
+</style>
---
+++ getsqlday.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Pull data from MySQL server and graph them.
+
+datastore="/tmp/kamstrupd/mysql"
+
+if [ ! -d "$datastore" ]; then
+  mkdir -p "$datastore"
+fi
+
+interval="INTERVAL 30 HOUR "
+# host=$(hostname)
+
+pushd "$HOME/kamstrupd" >/dev/null
+  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM kamstrup where (sample_time >=NOW() - $interval);" | sed 's/\t/;/g;s/\n//g' > "$datastore/kamd.csv"
+
+  #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
+  #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql2c.csv
+popd >/dev/null
