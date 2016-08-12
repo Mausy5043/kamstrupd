53d3120cb852bf7814a50c5812ba1a197c504982
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-08-12 09:08:24 +0200

    graphs script added

---
+++ graphs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Pull data from MySQL server and graph them.
+
+LOCAL=$(date)
+LOCALSECONDS=$(date -d "$LOCAL" +%s)
+UTC=$(date -u -d "$LOCAL" +"%Y-%m-%d %H:%M:%S")  #remove timezone reference
+UTCSECONDS=$(date -d "$UTC" +%s)
+UTCOFFSET=$((LOCALSECONDS - UTCSECONDS))
+
+pushd "$HOME/lnxdiagd" >/dev/null
+  if [ $(wc -l < /tmp/kamstrupd/mysql/kamd.csv) -gt 5 ]; then
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./kamactual.gp &
+  fi
+
+  wait
+
+popd >/dev/null
