2d8def45cf36dd8d8ac0f3ad17f0a1ad640f6bff
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-08-15 12:43:50 +0200

    graphing weekly

--- getsqlweek.sh
+++ getsqlweek.sh
@@ -13,7 +13,7 @@ interval="INTERVAL 8 DAY "
 
 pushd "$HOME/kamstrupd" >/dev/null
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM kamstrup where (sample_time >=NOW() - $interval);" | sed 's/\t/;/g;s/\n//g' > "$datastore/kamw.csv"
-
+  awk 'NR % 60 == 0' "$datastore/kamw.csv" > "$datastore/kamwr.csv"
   #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > $datastore/sql2c.csv
 popd >/dev/null
