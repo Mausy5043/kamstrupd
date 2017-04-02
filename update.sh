8aa114aadb8edf287f44c8262497dcf6385e2d72
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2017-04-02 19:07:57 +0200

    don't change permissions

--- update.sh
+++ update.sh
@@ -32,7 +32,7 @@ pushd "$HOME/kamstrupd"
   git checkout "$branch"
   git reset --hard "origin/$branch" && git clean -f -d
   # Set permissions
-  chmod -R 744 ./*
+  # chmod -R 744 ./*
 
   for fname in $DIFFLIST; do
     echo ">   $fname was updated from GIT"
@@ -45,19 +45,19 @@ pushd "$HOME/kamstrupd"
     fi
 
     # LIBDAEMON.PY changed
-    if [[ "$fname" == "libdaemon.py" ]]; then
-      echo "  ! Diagnostic library changed"
-      echo "  o Restarting all kam daemons"
-      for daemon in $kamlist; do
-        echo "  +- Restart kam$daemon"
-        eval "./kam$daemon"d.py restart
-      done
-      echo "  o Restarting all service daemons"
-      for daemon in $srvclist; do
-        echo "  +- Restart kam$daemon"
-        eval "./kam$daemon"d.py restart
-      done
-    fi
+    #if [[ "$fname" == "libdaemon.py" ]]; then
+    #  echo "  ! Diagnostic library changed"
+    #  echo "  o Restarting all kam daemons"
+    #  for daemon in $kamlist; do
+    #    echo "  +- Restart kam$daemon"
+    #    eval "./kam$daemon"d.py restart
+    #  done
+    #  echo "  o Restarting all service daemons"
+    #  for daemon in $srvclist; do
+    #    echo "  +- Restart kam$daemon"
+    #    eval "./kam$daemon"d.py restart
+    #  done
+    #fi
 
     #CONFIG.INI changed
     if [[ "$fname" == "config.ini" ]]; then
