e2b8395838dc864505ef8a7540d9ffcbfc773d3f
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-04-29 16:18:35 +0200

    stop waiting

--- update.sh
+++ update.sh
@@ -8,8 +8,8 @@ HOSTNAME=$(cat /etc/hostname)
 branch=$(cat "$HOME/.kamstrupd.branch")
 
 # Wait for the daemons to finish their job. Prevents stale locks when restarting.
-echo "Waiting 30s..."
-sleep 30
+#echo "Waiting 30s..."
+#sleep 30
 
 # make sure working tree exists
 if [ ! -d /tmp/kamstrupd/site/img ]; then
