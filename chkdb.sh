26fdf91d5b0413041c7876da15862f37fe78271b
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2017-07-14 20:55:03 +0200

    no .lan

--- chkdb.sh
+++ chkdb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "--Looking for entries with zero value counter--"
-mysql -h sql.lan -e "USE domotica; SELECT * FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
+mysql -h sql -e "USE domotica; SELECT * FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 echo
