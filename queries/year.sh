dcbaf87a6b9a16bfa49364a51faa97f3f2f0d5b2
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-08-24 11:05:29 +0200

    split off year query from week

--- queries/year.sh
+++ queries/year.sh
@@ -11,8 +11,9 @@ fi
 # host=$(hostname)
 
 pushd "$HOME/kamstrupd" >/dev/null
-  #week (per hour = 60')
-  interval="INTERVAL 8 DAY "
+  #year (per week = 10080')
+  # 7d*24h*60m = 10080m
+  interval="INTERVAL 370 DAY "
   time mysql -h sql --skip-column-names -e     \
   "USE domotica;                          \
   SELECT *                                \
@@ -20,19 +21,7 @@ pushd "$HOME/kamstrupd" >/dev/null
   WHERE (sample_time >=NOW() - $interval) \
   ;"                                      \
   | sed 's/\t/;/g;s/\n//g'                \
-  | awk 'NR % 60 == 0' > "$datastore/kamwr.csv"
-
-  #month (per day =  1440')
-  # 24h*60m = 1440m
-  interval="INTERVAL 32 DAY "
-  time mysql -h sql --skip-column-names -e     \
-  "USE domotica;                          \
-  SELECT *                                \
-  FROM kamstrup                           \
-  WHERE (sample_time >=NOW() - $interval) \
-  ;"                                      \
-  | sed 's/\t/;/g;s/\n//g'                \
-  | awk 'NR % 1440 == 0' > "$datastore/kammr.csv"
+  | awk 'NR % 10080 == 0' > "$datastore/kamyr.csv"
 
   #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > $datastore/sql2c.csv
