399238fcfa7bc7500b317f9b6a1f04142ed141b2
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2017-08-24 09:30:01 +0200

    increased readability

--- getsqlhour.sh
+++ getsqlhour.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Pull data from MySQL server and graph them.
+# Pull data from MySQL server.
 
 datastore="/tmp/kamstrupd/mysql"
 
@@ -12,7 +12,13 @@ interval="INTERVAL 70 MINUTE "
 # host=$(hostname)
 
 pushd "$HOME/kamstrupd" >/dev/null
-  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM kamstrup where (sample_time >=NOW() - $interval);" | sed 's/\t/;/g;s/\n//g' > "$datastore/kamh.csv"
+  mysql -h sql --skip-column-names -e       \
+  "USE domotica;                            \
+  SELECT *                                  \
+  FROM kamstrup                             \
+  WHERE (sample_time >=NOW() - $interval)   \
+  ;"                                        \
+  | sed 's/\t/;/g;s/\n//g' > "$datastore/kamh.csv"
 
   #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > $datastore/sql2c.csv
