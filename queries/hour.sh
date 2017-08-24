f74af86d6dab79fb9116c7df9411e8193b746374
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-08-24 11:42:34 +0200

    hour test query added

--- queries/hour.sh
+++ queries/hour.sh
@@ -14,11 +14,23 @@ interval="INTERVAL 70 MINUTE "
 pushd "$HOME/kamstrupd" >/dev/null
   time mysql -h sql --skip-column-names -e       \
   "USE domotica;                            \
-  SELECT *                                  \
-  FROM kamstrup                             \
-  WHERE (sample_time >=NOW() - $interval)   \
+   SELECT *                                  \
+    FROM kamstrup                             \
+    WHERE (sample_time >=NOW() - $interval)   \
   ;"                                        \
   | sed 's/\t/;/g;s/\n//g' > "$datastore/kamh.csv"
+
+  time mysql -h sql --skip-column-names -e       \
+  "USE domotica;                             \
+   SELECT MIN(sample_epoch),                 \
+          MAX(T1in)                          \
+          MAX(T2in)                          \
+          AVG(powerin)                       \
+    FROM kamstrup                            \
+    WHERE (sample_time >=NOW() - $interval)  \
+    GROUP_BY (sample_epoch DIV 60)           \
+   ;"                                        \
+  | sed 's/\t/;/g;s/\n//g' > "$datastore/kamh2.csv"
 
   #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > $datastore/sql2c.csv
