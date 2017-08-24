bd1b2e59b07cd9aac97995378ef6d9e0464c4714
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-08-24 11:48:11 +0200

    add test script for day

--- queries/day.sh
+++ queries/day.sh
@@ -20,6 +20,18 @@ pushd "$HOME/kamstrupd" >/dev/null
   ;"                                      \
   | sed 's/\t/;/g;s/\n//g' > "$datastore/kamd.csv"
 
+  time mysql -h sql --skip-column-names -e       \
+  "USE domotica;                             \
+   SELECT MIN(sample_epoch),                 \
+          MAX(T1in),                         \
+          MAX(T2in),                         \
+          AVG(powerin)                       \
+    FROM kamstrup                            \
+    WHERE (sample_time >=NOW() - $interval)  \
+    GROUP BY (sample_epoch DIV 1800)           \
+   ;"                                        \
+  | sed 's/\t/;/g;s/\n//g' > "$datastore/kamd2.csv"
+
   #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql2c.csv
 popd >/dev/null
