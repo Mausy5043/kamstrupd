bf19e6fedf9977f0890b454152668dfb48c72475
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-08-25 10:01:06 +0200

    include delete function

--- chkdb.sh
+++ chkdb.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+argv=("$@")
+
 echo "--Looking for entries with zero value counter--"
 mysql -h sql -e "USE domotica; SELECT * FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 echo
+
+if [[ ${argv[0]} == "-d" ]]; then
+  echo "Deleting data in 5s"
+  sleep 5 && mysql -h sql -e "USE domotica; DELETE FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
+fi
