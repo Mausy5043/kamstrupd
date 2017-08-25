23d712b98ab3c279e2c12cea6cdc2a9dc321f0d4
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-08-25 10:25:55 +0200

    use bash iso sh

--- chkdb.sh
+++ chkdb.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 arg1="$1"
 
@@ -6,7 +6,7 @@ echo "--Looking for entries with zero value counter--"
 mysql -h sql -e "USE domotica; SELECT * FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 echo
 
-if [[ "$arg1" == "-d" ]]; then
+if [[ "$arg1" = "-d" ]]; then
   echo "Deleting data in 5s"
   sleep 5 && mysql -h sql -e "USE domotica; DELETE FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 fi
