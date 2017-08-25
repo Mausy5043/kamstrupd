2c201ecc7a3ef46705eaf03a27ef0022eb4f9022
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-08-25 10:12:28 +0200

    debug

--- chkdb.sh
+++ chkdb.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-argv=("$@")
+arg1="$1"
 
 echo "--Looking for entries with zero value counter--"
 mysql -h sql -e "USE domotica; SELECT * FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 echo
 
-if [[ ${argv[0]} == "-d" ]]; then
+if [[ "$arg1" == "-d" ]]; then
   echo "Deleting data in 5s"
   sleep 5 && mysql -h sql -e "USE domotica; DELETE FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 fi
