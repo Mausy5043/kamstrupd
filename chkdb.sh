001d801667f469298ab40c40000c25c15274e586
Mausy5043 <Mausy5043@users.noreply.github.com>
2018-04-01 11:08:45 +0200

    add instruction

--- chkdb.sh
+++ chkdb.sh
@@ -9,4 +9,8 @@ echo
 if [[ "$arg1" = "-d" ]]; then
   echo "Deleting data in 5s"
   sleep 5 && mysql -h sql -e "USE domotica; DELETE FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
+else
+  echo "To remove zero value entries from the database, use:"
+  echo "chkdb.sh -d"
+  echo
 fi
