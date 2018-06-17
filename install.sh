afbf5fcfd4e559236c9edd6a7902fd736343a4cd
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2018-06-17 09:14:26 +0000

    upgrade lib requirement

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 # a `*boot` repo.
 
 ME=$(whoami)
-required_commonlibversion="0.4.2"
-commonlibbranch="v0_4"
+required_commonlibversion="0.5.4"
+commonlibbranch="v0_5"
 
 echo -n "Started installing KAMSTRUPd on "; date
 minit=$(echo $RANDOM/555 |bc)
