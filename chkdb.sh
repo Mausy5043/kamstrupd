973d2a52de33c366749d3609f35d0f15f1ae9cf6
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2018-07-29 16:48:04 +0200

    fix path

--- chkdb.sh
+++ chkdb.sh
@@ -8,7 +8,7 @@ echo
 
 if [[ "$arg1" = "-d" ]]; then
   echo "Deleting data in 5s"
-  sleep 5 && mysql -h sql -e "USE domotica; DELETE FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
+  sleep 5 && mysql --defaults-file="~/.my.kam.cnf" -h sql -e "USE domotica; DELETE FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 else
   echo "To remove zero value entries from the database, use:"
   echo "chkdb.sh -d"
