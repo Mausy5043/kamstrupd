16f4c8acff3c8ad1c829444b3c097d7ffcef0d6f
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2018-09-10 16:44:37 +0200

    use defaults file new file:   .idea/vcs.xml

--- chkdb.sh
+++ chkdb.sh
@@ -3,11 +3,11 @@
 arg1="$1"
 
 echo "--Looking for entries with zero value counter--"
-mysql -h sql -e "USE domotica; SELECT * FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
+mysql --defaults-file="~/.my.kam.cnf" -h sql -e "USE domotica; SELECT * FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 echo
 
 if [[ "$arg1" = "-d" ]]; then
-  echo "Deleting data in 5s"
+  echo "Deleting data in 5s.."
   sleep 5 && mysql --defaults-file="~/.my.kam.cnf" -h sql -e "USE domotica; DELETE FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 else
   echo "To remove zero value entries from the database, use:"
