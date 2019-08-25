30d4aba6ade2d03d6613000c2b5b5563b198a2c1
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-08-25 13:04:26 +0200

    convert to sqlite3

--- chkdb.sh
+++ chkdb.sh
@@ -3,12 +3,12 @@
 arg1="$1"
 
 echo "--Looking for entries with zero value counter--"
-mysql --defaults-file="~/.my.kam.cnf" -h boson -e "USE domotica; SELECT * FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
+sqlite3 "${HOME}/.sqlite3/electriciteit.sqlite3" "SELECT * FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 echo
 
 if [[ "$arg1" = "-d" ]]; then
   echo "Deleting data in 5s.."
-  sleep 5 && mysql --defaults-file="~/.my.kam.cnf" -h boson -e "USE domotica; DELETE FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
+  sleep 5 && sqlite3 "${HOME}/.sqlite3/electriciteit.sqlite3" "DELETE FROM kamstrup WHERE (T1in = 0 OR T2in = 0);"
 else
   echo "To remove zero value entries from the database, use:"
   echo "chkdb.sh -d"
