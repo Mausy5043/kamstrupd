ccd7ff8bdda3e053f8533b4b1dd1c8761951e12b
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-08-31 16:55:59 +0200

    debug query

--- queries/hour.sh
+++ queries/hour.sh
@@ -14,7 +14,7 @@ divisor="3600"
 pushd "$HOME/kamstrupd" >/dev/null || exit 1
   # totals per hour for T1in, T2in, T1out, T2out
   sqlite3 "${HOME}.sqlite3/electriciteit.sqlite3" \
-     ".separator '. '"
+     ".separator '. '" \
      "SELECT strftime('%H',sample_time), \
              MAX(T1in)-MIN(T1in), \
              MAX(T2in)-MIN(T2in), \
