fb037eee2ec29b312288bf3dfb6c67ddc6bcee1b
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-09-01 20:30:23 +0200

    read a file

--- scripts/kam41.py
+++ scripts/kam41.py
@@ -22,6 +22,41 @@ MYAPP       = os.path.realpath(__file__).split('/')[-3]
 NODE        = os.uname()[1]
 
 
+def get_cli_params():
+    """Check for presence of a CLI parameter."""
+    if len(sys.argv) != 2:
+        sys.exit(0)
+    # 1 parameter required = filename to be processed
+    return sys.argv[1]
+
+
+def read_file(file_to_read_from):
+    """
+    Return the contents of a file if it exists.
+
+    Abort if it doesn't exist.
+    """
+    if not os.path.isfile(file_to_read_from):
+        sys.exit(0)
+    with open(file_to_read_from, 'r') as input_file:
+        # read the inputfile
+        return input_file.read().splitlines()
+
+
+def write_file(file_to_write_to, lines_to_write):
+    """
+    Output <lines_to_write> to the file <file_to_write_to>.
+
+    Will overwrite existing file.
+    """
+    with open(file_to_write_to, 'w') as output_file:
+        for line in lines_to_write:
+            output_file.write(f'{line}\n')
+
+
 if __name__ == "__main__":
   # initialise logging
   syslog.openlog(ident=MYAPP, facility=syslog.LOG_LOCAL0)
+  ifile = get_cli_params()
+  lines = read_file(ifile)
+  print(lines.split(', '))
