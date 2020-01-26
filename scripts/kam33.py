7244a967a0adc13028cecbdd5d28e61f703b6aae
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2020-01-26 11:34:29 +0100

    adjust trending to new data format

--- scripts/kam33.py
+++ scripts/kam33.py
@@ -59,9 +59,6 @@ def fetch_last_day():
     ...
     """
   trend_T_data, trend_S_data, data_lbls = get_historic_data('%d %Hh', 50, 'hour')
-  print(data_lbls)
-  print(trend_T_data)
-  print(trend_S_data)
   return data_lbls, trend_T_data, trend_S_data
 
 
@@ -108,50 +105,19 @@ def plot_graph(output_file, data_tuple, plot_title):
   plt.rc('font', size=13)
   dummy, ax1 = plt.subplots(1, figsize=(20, 7))
 
-  # Create a bar plot of import_lo
-  ax1.bar(tick_pos, import_hi,
-          width=bar_width,
-          label='Import (T2)',
-          alpha=ahpla,
-          color='y',
-          align='center',
-          bottom=[sum(i) for i in zip(import_lo, own_usage)]
-          )
-  # Create a bar plot of import_hi
-  ax1.bar(tick_pos, import_lo,
-          width=bar_width,
-          label='Import (T1)',
-          alpha=ahpla,
-          color='b',
-          align='center',
-          bottom=own_usage
-          )
   # Create a bar plot of usage_slf
-  ax1.bar(tick_pos, own_usage,
+  ax1.bar(tick_pos, trend_S_data,
           width=bar_width,
-          label='Self',
-          alpha=ahpla,
-          color='g',
-          align='center'
-          )
-  # Exports hang below the y-axis
-  # Create a bar plot of export_lo
-  ax1.bar(tick_pos, [-1 * i for i in export_lo],
-          width=bar_width,
-          label='Export (T1)',
-          alpha=ahpla,
-          color='c',
-          align='center'
-          )
-  # Create a bar plot of export_hi
-  ax1.bar(tick_pos, [-1 * i for i in export_hi],
-          width=bar_width,
-          label='Export (T2)',
+          label='Zonnekracht',
           alpha=ahpla,
           color='r',
-          align='center',
-          bottom=[-1 * i for i in export_lo]
+          align='center'
           )
+  ax1.line(tick_pos, trend_T_data,
+           label='Temperatuur',
+           color='k',
+           align='center'
+           )
 
   # Set Axes stuff
   ax1.set_ylabel("[kWh]")
