4f814f1e2caa9a62b315257c89894de85b1be268
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2020-02-24 21:32:22 +0100

    remove legacy code

--- scripts/pastmonth.sh
+++ scripts/pastmonth.sh
@@ -1,37 +1,10 @@
 #!/bin/bash
 
-# query daily totals for a period of one month (744 hours)
-
-# interval="-1 month"
+# query daily totals for a period of one month
 
 pushd "${HOME}/kamstrupd" >/dev/null || exit 1
-#shellcheck disable=SC1091
-# source ./scripts/environment.sh
-# totals per hour for T1in, T2in, T1out, T2out
-
-# #shellcheck disable=SC2154
-# sqlite3 "${HOME}/.sqlite3/electriciteit.sqlite3" \
-#    ".separator '; '" \
-#    "SELECT strftime('%m-%d',sample_time) as sol, \
-#            (MAX(T1in)-MIN(T1in))/1000, \
-#            (MAX(T2in)-MIN(T2in))/1000, \
-#            (MAX(T1out)-MIN(T1out))/1000, \
-#            (MAX(T2out)-MIN(T2out))/1000, \
-#            MIN(sample_epoch) as t \
-#     FROM kamstrup \
-#     WHERE (sample_time >= datetime('now', '${interval}')) \
-#     GROUP BY sol \
-#     ORDER BY t ASC \
-#     ;" > "${kamdata}"
-
-# if [ "$(wc -l < "${kamdata}")" -gt 5 ]; then
-#   timeout 120s gnuplot -e "utc_offset='${UTCOFFSET}'; kamdata='${kamdata}'" ./graphs/pastmonth.gp
-# fi
 
 ./scripts/kam43.py -m
 ./scripts/upload.sh --upload
-
-# drop datafile
-# rm "${kamdata}"
 
 popd >/dev/null || exit
--- scripts/pastyear.sh
+++ scripts/pastyear.sh
@@ -2,40 +2,12 @@
 
 # query monthly totals for a period of n years
 
-# interval="-61 month"
-
 pushd "${HOME}/kamstrupd" >/dev/null || exit 1
-#shellcheck disable=SC1091
-# source ./scripts/environment.sh
-# totals per hour for T1in, T2in, T1out, T2out
-
-# #shellcheck disable=SC2154
-# sqlite3 "${HOME}/.sqlite3/electriciteit.sqlite3" \
-#    ".separator '; '" \
-#    "SELECT strftime('%Y-%m',sample_time) as annomoon, \
-#            (MAX(T1in)-MIN(T1in))/1000, \
-#            (MAX(T2in)-MIN(T2in))/1000, \
-#            (MAX(T1out)-MIN(T1out))/1000, \
-#            (MAX(T2out)-MIN(T2out))/1000, \
-#            strftime('%Y',sample_time), \
-#            strftime('%m',sample_time) \
-#     FROM kamstrup \
-#     WHERE (sample_time >= datetime('now', '${interval}')) \
-#     GROUP BY annomoon \
-#     ORDER BY annomoon ASC \
-#     ;" > "${kamdata}"
-
-# if [ "$(wc -l < "${kamdata}")" -gt 5 ]; then
-#   timeout 120s gnuplot -e "utc_offset='${UTCOFFSET}'; kamdata='${kamdata}'" ./graphs/pastyear.gp
-# fi
 
 ./scripts/kam43.py -y
 ./scripts/kam44.py -m
-./scripts/upload.sh --upload
-
-# # drop datafile
-# rm "${kamdata}"
-
 ./scripts/avgday.sh
+
+./scripts/upload.sh --upload
 
 popd >/dev/null || exit
