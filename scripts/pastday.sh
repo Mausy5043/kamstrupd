197a05aa35d279d5896b7d0a2e8769dcd7625000
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2020-08-17 18:29:50 +0200

    re-enable daily tracking

--- scripts/pastday.sh
+++ scripts/pastday.sh
@@ -3,8 +3,6 @@
 # query hourly totals for a period of two days
 
 pushd "${HOME}/kamstrupd" >/dev/null || exit 1
-
-#./scripts/kam43.py -d
-#./scripts/upload.sh --upload
-
+  ./scripts/kam43.py -d
+  ./scripts/upload.sh --upload
 popd >/dev/null || exit
