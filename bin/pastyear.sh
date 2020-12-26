3f15ec15fdbda55cfd3065c45e8227fbee0d7af0
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2020-12-26 14:24:52 +0100

    specify the year to gauge

--- bin/pastyear.sh
+++ bin/pastyear.sh
@@ -5,7 +5,7 @@
 HERE=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 
 pushd "${HERE}" >/dev/null || exit 1
-    ./kam44.py --gauge 0 &
+    ./kam44.py --gauge &
     ./kam43.py --months 0 &
     ./kam43.py --years 0 &
     ./kam44.py --months 0 &
