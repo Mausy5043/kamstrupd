5e7e7f5036dc6387f2c5343f8f388c1a1a6bc6d3
Maurice Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2021-06-27 12:04:22 +0200

    auto-formatted

--- bin/kamstrup.py
+++ bin/kamstrup.py
@@ -22,13 +22,17 @@ import serial
 
 parser = argparse.ArgumentParser(description="Execute the telemetry daemon.")
 parser_group = parser.add_mutually_exclusive_group(required=True)
-parser_group.add_argument('--start', action='store_true', help='start the daemon as a service')
-parser_group.add_argument('--debug', action='store_true', help='start the daemon in debugging mode')
+parser_group.add_argument(
+    "--start", action="store_true", help="start the daemon as a service"
+)
+parser_group.add_argument(
+    "--debug", action="store_true", help="start the daemon in debugging mode"
+)
 OPTION = parser.parse_args()
 
 # constants
 DEBUG = False
-HERE = os.path.realpath(__file__).split('/')
+HERE = os.path.realpath(__file__).split("/")
 # runlist id :
 MYID = HERE[-1]
 # app_name :
@@ -62,9 +66,11 @@ def main():
     iniconf.read(f"{os.environ['HOME']}/{MYAPP}/config.ini")
     report_time = iniconf.getint(MYID, "reporttime")
     fdatabase = f"{os.environ['HOME']}/{iniconf.get(MYID, 'databasefile')}"
-    sqlcmd = iniconf.get(MYID, 'sqlcmd')
-    samples_averaged = iniconf.getint(MYID, 'samplespercycle') * iniconf.getint(MYID, 'cycles')
-    sample_time = report_time / iniconf.getint(MYID, 'samplespercycle')
+    sqlcmd = iniconf.get(MYID, "sqlcmd")
+    samples_averaged = iniconf.getint(
+        MYID, "samplespercycle"
+    ) * iniconf.getint(MYID, "cycles")
+    sample_time = report_time / iniconf.getint(MYID, "samplespercycle")
     data = []
 
     test_db_connection(fdatabase)
@@ -76,7 +82,7 @@ def main():
         if time.time() > pause_time:
             start_time = time.time()
             result = do_work()
-            result = result.split(',')
+            result = result.split(",")
             mf.syslog_trace(f"Result   : {result}", False, DEBUG)
             # data.append(list(map(int, result)))
             data.append([int(d) for d in result])
@@ -98,20 +104,34 @@ def main():
                 if averages[0] > 0:
                     do_add_to_database(averages, fdatabase, sqlcmd)
 
-            pause_time = (sample_time
-                          - (time.time() - start_time)
-                          - (start_time % sample_time)
-                          + time.time())
+            pause_time = (
+                sample_time
+                - (time.time() - start_time)
+                - (start_time % sample_time)
+                + time.time()
+            )
             if pause_time > 0:
-                mf.syslog_trace(f"Waiting  : {pause_time - time.time():.1f}s", False, DEBUG)
+                mf.syslog_trace(
+                    f"Waiting  : {pause_time - time.time():.1f}s",
+                    False,
+                    DEBUG,
+                )
                 # no need to wait for the next cycles
                 # the meter will pace the meaurements
                 # any required waiting will be inside gettelegram()
                 # time.sleep(pause_time)
-                mf.syslog_trace("................................", False, DEBUG)
+                mf.syslog_trace(
+                    "................................", False, DEBUG
+                )
             else:
-                mf.syslog_trace(f"Behind   : {pause_time - time.time():.1f}s", False, DEBUG)
-                mf.syslog_trace("................................", False, DEBUG)
+                mf.syslog_trace(
+                    f"Behind   : {pause_time - time.time():.1f}s",
+                    False,
+                    DEBUG,
+                )
+                mf.syslog_trace(
+                    "................................", False, DEBUG
+                )
         else:
             time.sleep(1.0)
 
@@ -126,51 +146,59 @@ def do_work():
     global powerout
     global tarif
     global swits
-    # swits is not always present. The value will change *if* present in the telegram.
+    # swits is not always present. The value will
+    # change *if* present in the telegram.
     swits = 0
     telegram, status = gettelegram()
 
     if status == 1:
         for element in telegram:
             try:
-                line = re.split(r'[\(\*\)]', element)  # noqa
+                line = re.split(r"[\(\*\)]", element)  # noqa
                 # ['1-0:1.8.1', '00175.402', 'kWh', '']  T1 in
-                if line[0] == '1-0:1.8.1':
+                if line[0] == "1-0:1.8.1":
                     electra1in = int(float(line[1]) * 1000)
                 # ['1-0:1.8.2', '00136.043', 'kWh', '']  T2 in
-                if line[0] == '1-0:1.8.2':
+                if line[0] == "1-0:1.8.2":
                     electra2in = int(float(line[1]) * 1000)
                 # ['1-0:2.8.1', '00000.000', 'kWh', '']  T1 out
-                if line[0] == '1-0:2.8.1':
+                if line[0] == "1-0:2.8.1":
                     electra1out = int(float(line[1]) * 1000)
                 # ['1-0:2.8.2', '00000.000', 'kWh', '']  T2 out
-                if line[0] == '1-0:2.8.2':
+                if line[0] == "1-0:2.8.2":
                     electra2out = int(float(line[1]) * 1000)
                 # ['0-0:96.14.0', '0002', '']  tarif 1 or 2
-                if line[0] == '0-0:96.14.0':
+                if line[0] == "0-0:96.14.0":
                     tarif = int(line[1])
                 # ['1-0:1.7.0', '0000.32', 'kW', '']  power in
-                if line[0] == '1-0:1.7.0':
+                if line[0] == "1-0:1.7.0":
                     powerin = int(float(line[1]) * 1000)
                 # ['1-0:2.7.0', '0000.00', 'kW', ''] power out
-                if line[0] == '1-0:2.7.0':
+                if line[0] == "1-0:2.7.0":
                     powerout = int(float(line[1]) * 1000)
                 # ['0-0:17.0.0', '999', 'A', ''] unknown; not recorded
-                # ['0-0:96.3.10', '1', '']  powerusage (1) or powermanufacturing ()
-                if line[0] == '0-0:96.3.10':
+                # ['0-0:96.3.10', '1', '']  powerusage (1)
+                #                           or powermanufacturing ()
+                if line[0] == "0-0:96.3.10":
                     swits = int(line[1])
                 # ['0-0:96.13.1', '', '']
                 # not recorded
                 # ['0-0:96.13.0', '', '']
                 # not recorded
             except ValueError:
-                mf.syslog_trace("*** Conversion not possible for element:", syslog.LOG_CRIT, DEBUG)
+                mf.syslog_trace(
+                    "*** Conversion not possible for element:",
+                    syslog.LOG_CRIT,
+                    DEBUG,
+                )
                 mf.syslog_trace(f"    {element}", syslog.LOG_CRIT, DEBUG)
-                mf.syslog_trace("*** Extracted from telegram:", syslog.LOG_DEBUG, DEBUG)
+                mf.syslog_trace(
+                    "*** Extracted from telegram:", syslog.LOG_DEBUG, DEBUG
+                )
                 mf.syslog_trace(f"    {telegram}", syslog.LOG_DEBUG, DEBUG)
                 pass
 
-    return f'{electra1in}, {electra2in}, {powerin}, {electra1out}, {electra2out}, {powerout}, {tarif}, {swits}'
+    return f"{electra1in}, {electra2in}, {powerin}, {electra1out}, {electra2out}, {powerout}, {tarif}, {swits}"
 
 
 # noinspection PyUnresolvedReferences
@@ -189,17 +217,21 @@ def gettelegram():
     while abort == 0:
         try:
             # line = "".join(iter(lambda: PORT.read(1), delim)).strip()
-            line = str(PORT.readline().strip(), 'utf-8')
+            line = str(PORT.readline().strip(), "utf-8")
             if line == "!":
                 abort = 1
             if line != "":
                 telegram.append(line)
         except serial.SerialException:
-            mf.syslog_trace("*** Serialport read error:", syslog.LOG_CRIT, DEBUG)
+            mf.syslog_trace(
+                "*** Serialport read error:", syslog.LOG_CRIT, DEBUG
+            )
             mf.syslog_trace(traceback.format_exc(), syslog.LOG_CRIT, DEBUG)
             abort = 2
         except UnicodeDecodeError:
-            mf.syslog_trace("*** Unicode Decode error:", syslog.LOG_CRIT, DEBUG)
+            mf.syslog_trace(
+                "*** Unicode Decode error:", syslog.LOG_CRIT, DEBUG
+            )
             mf.syslog_trace(traceback.format_exc(), syslog.LOG_CRIT, DEBUG)
             abort = 2
 
@@ -212,9 +244,9 @@ def gettelegram():
         abort = 2
 
     if abort == 1:
-        with open('/tmp/kamstrup.raw', 'w') as output_file:
+        with open("/tmp/kamstrup.raw", "w") as output_file:
             for line in telegram:
-                output_file.write(f'{line}\n')
+                output_file.write(f"{line}\n")
 
     # Return codes:
     # abort == 1 indicates a successful read
@@ -228,13 +260,21 @@ def do_add_to_database(result, fdatabase, sql_cmd):
     # Get the time and date in human-readable form and UN*X-epoch...
     conn = None
     cursor = None
-    dt_format = '%Y-%m-%d %H:%M:%S'
+    dt_format = "%Y-%m-%d %H:%M:%S"
     out_date = dt.datetime.now()  # time.strftime('%Y-%m-%dT%H:%M:%S')
     out_epoch = int(out_date.timestamp())
-    results = (out_date.strftime(dt_format), out_epoch,
-               result[0], result[1], result[2],
-               result[3], result[4], result[5],
-               result[6], result[7])
+    results = (
+        out_date.strftime(dt_format),
+        out_epoch,
+        result[0],
+        result[1],
+        result[2],
+        result[3],
+        result[4],
+        result[5],
+        result[6],
+        result[7],
+    )
     mf.syslog_trace(f"   @: {out_date.strftime(dt_format)}", False, DEBUG)
     mf.syslog_trace(f"    : {results}", False, DEBUG)
 
@@ -257,13 +297,15 @@ def do_add_to_database(result, fdatabase, sql_cmd):
 
 def create_db_connection(database_file):
     """
-    Create a database connection to the SQLite3 database specified by database_file.
+    Create a database connection to the SQLite3 database specified
+    by database_file.
     """
     consql = None
     mf.syslog_trace(f"Connecting to: {database_file}", False, DEBUG)
     try:
         consql = sqlite3.connect(database_file, timeout=9000)
-        # if consql:    # dB initialised succesfully -> get a cursor on the dB and run a test.
+        # if consql:    # dB initialised succesfully -> get a cursor on the dB
+        #                                               and run a test.
         #  cursql = consql.cursor()
         #  cursql.execute("SELECT sqlite_version()")
         #  versql = cursql.fetchone()
@@ -272,11 +314,17 @@ def create_db_connection(database_file):
         #  syslog.syslog(syslog.LOG_INFO, logtext)
         return consql
     except sqlite3.Error:
-        mf.syslog_trace("Unexpected SQLite3 error when connecting to server.", syslog.LOG_CRIT, DEBUG)
+        mf.syslog_trace(
+            "Unexpected SQLite3 error when connecting to server.",
+            syslog.LOG_CRIT,
+            DEBUG,
+        )
         mf.syslog_trace(traceback.format_exc(), syslog.LOG_CRIT, DEBUG)
         if consql:  # attempt to close connection to SQLite3 server
             consql.close()
-            mf.syslog_trace(" ** Closed SQLite3 connection. **", syslog.LOG_CRIT, DEBUG)
+            mf.syslog_trace(
+                " ** Closed SQLite3 connection. **", syslog.LOG_CRIT, DEBUG
+            )
         raise
 
 
@@ -292,16 +340,22 @@ def test_db_connection(fdatabase):
         cursor.close()
         conn.commit()
         conn.close()
-        syslog.syslog(syslog.LOG_INFO, f"Attached to SQLite3 server: {versql}")
+        syslog.syslog(
+            syslog.LOG_INFO, f"Attached to SQLite3 server: {versql}"
+        )
     except sqlite3.Error:
-        mf.syslog_trace("Unexpected SQLite3 error during test.", syslog.LOG_CRIT, DEBUG)
+        mf.syslog_trace(
+            "Unexpected SQLite3 error during test.", syslog.LOG_CRIT, DEBUG
+        )
         mf.syslog_trace(traceback.format_exc(), syslog.LOG_CRIT, DEBUG)
         raise
 
 
 if __name__ == "__main__":
     # initialise logging
-    syslog.openlog(ident=f'{MYAPP}.{MYID.split(".")[0]}', facility=syslog.LOG_LOCAL0)
+    syslog.openlog(
+        ident=f'{MYAPP}.{MYID.split(".")[0]}', facility=syslog.LOG_LOCAL0
+    )
 
     # noinspection PyUnresolvedReferences
     PORT = serial.Serial()  # pylint: disable=C0103
@@ -316,7 +370,7 @@ if __name__ == "__main__":
     PORT.rtscts = 0
     PORT.dsrdtr = 0
     PORT.timeout = 15
-    PORT.port = '/dev/ttyUSB0'
+    PORT.port = "/dev/ttyUSB0"
 
     if OPTION.debug:
         DEBUG = True
