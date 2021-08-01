7489b7bfdbef508c4347246c6ae63cf44a5e01ef
Maurice Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2021-08-01 13:54:37 +0200

    removing unused code

--- bin/zappi.py
+++ bin/zappi.py
@@ -27,65 +27,6 @@ CONFIG_FILE = os.environ["HOME"] + "/.config/kamstrup/key.ini"
 OPTION = ""
 
 
-def str2int(arr):
-    for k, v in enumerate(arr):
-        arr[k] = int(v)
-    return arr
-
-
-def int2str(arr):
-    for k, v in enumerate(arr):
-        arr[k] = str(v).zfill(2)
-    return arr
-
-
-# def fetch_last_day(hours_to_fetch):
-#     # zappi_status = myenergi.get_status(
-#     #     f"cgi-jstatus-Z{myenergi.zappi_serial}"
-#     # )
-#     # zdate = str2int(zappi_status["zappi"][0]["dat"].split("-"))
-#     # ztime = str2int(zappi_status["zappi"][0]["tim"].split(":"))
-#     # zdst = int(zappi_status["zappi"][0]["dst"])
-
-#     # time_dict = {"hours": hours_to_fetch + 2}
-#     # time_delta = datetime.timedelta(**time_dict)
-#     # time_obj = datetime.datetime.now() - time_delta
-
-#     # ztime[0] -= hours_to_fetch
-#     # if ztime[0] < 0:
-#     #     ztime[0] += 24
-#     #     zdate[0] -= 1
-#     #     if zdate[1] < 1:
-#     #         zdate[1] += 12
-#     #         zdate[2] -= 1
-#     # zdate = int2str(zdate)
-#     # ztime = int2str(ztime)
-
-#     zappi_data = myenergi.get_status(
-#         f"cgi-jdayhour-Z{myenergi.zappi_serial}-{time_obj.year}-{time_obj.month}-{time_obj.day}-{time_obj.hour}"
-#     )
-#     # for key, value in enumerate(zappi_status["zappi"][0]):
-#     #     print(key, value, zappi_status["zappi"][0][value])
-#     print("")
-#     data_lbls = list()
-#     imp = list()
-#     gep = list()
-#     gen = list()
-#     exp = list()
-#     h1b = list()
-#     h1d = list()
-#     for key, value in enumerate(zappi_data[f"U{myenergi.zappi_serial}"]):
-#         block_values = myenergi.trans_data_block(value, zdst)
-#         data_lbls.append(block_values[0])
-#         imp.append(block_values[1])
-#         gep.append(block_values[2])
-#         gen.append(block_values[3])
-#         exp.append(block_values[4])
-#         h1b.append(block_values[5])
-#         h1d.append(block_values[6])
-#     return data_lbls, imp, gep, gen, exp, h1b, h1d
-
-
 def plot_graph(output_file, data_tuple, plot_title):
     """
     ...
@@ -96,7 +37,7 @@ def plot_graph(output_file, data_tuple, plot_title):
     data_lbls = data_tuple[0]
     importd = data_tuple[1]  # imp = P1 totaliser import
     opwekking = data_tuple[2]  # gep; PV production
-    green = data_tuple[3]  # gen; own use
+    green = data_tuple[3]  # gen; PV own use
     exportd = data_tuple[4]  # exp = P1 totaliser export
     h1b = data_tuple[5]  # h1b = EV (imported)
     h1d = data_tuple[6]  # h1d = EV (from PV)
@@ -108,7 +49,6 @@ def plot_graph(output_file, data_tuple, plot_title):
     own_usage = kl.distract(opwekking, exportd)
 
     own_usage = kl.distract(own_usage, h1b)
-    # usage = kl.contract(own_usage, imprt)
     if OPTION.print or DEBUG:
         print("LBLS", data_lbls)
         print("TIME", utc_dt)
